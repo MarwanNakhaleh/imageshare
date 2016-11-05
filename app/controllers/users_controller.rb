93bb6dacb35a5890b1a66119b0c243daece7a261
Marwan Nakhaleh <mnakhaleh@gmail.com>
2016-11-04 22:19:56 -0500

    basic authentication working

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,31 @@
+class UsersController < ApplicationController
+	def index
+		@users = User.all
+	end
+
+	def new
+	end
+
+	def create
+		user = User.new(user_params)
+		if user.save
+			session[:user_id] = user.id
+			redirect_to root_path
+		else
+			redirect_to signup_path
+		end
+	end
+
+	private
+	def user_params
+		params.require(:user).permit(
+			:username,
+			:first_name,
+			:last_name,
+			:email,
+			:dob,
+			:password,
+			:password_confirmation
+			)
+	end
+end
---
+++ test/controllers/pages_controller_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+class PagesControllerTest < ActionDispatch::IntegrationTest
+  test "should get dashboard" do
+    get pages_dashboard_url
+    assert_response :success
+  end
+
+  test "should get tutorial" do
+    get pages_tutorial_url
+    assert_response :success
+  end
+
+end
